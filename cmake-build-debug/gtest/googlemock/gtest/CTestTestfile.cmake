5b99182087b74158d74edba598db1dbc99c2f8e5
icsl-Jeon <junbs95@gmail.com>
2020-04-12 20:37:44 +0900

    clion build folder deleted

--- cmake-build-debug/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/jbs/catkin_ws/src/atypical_driving_snu/cmake-build-debug/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
