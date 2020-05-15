e1ed93a8530d61ba89937d9d119a6684b69ac490
lyw <yunwoo333@gmail.com>
2020-05-16 07:55:00 +0900

    No moonbeop error, but optimization incomplete

--- src/LocalPlanner.cpp
+++ src/LocalPlanner.cpp
@@ -5,7 +5,8 @@
 #include <atypical_planner/LocalPlanner.h>
 #include <optimization_module/problem.hpp>
 #include <optimization_module/dimension.h>
-
+#include <math.h>
+#include <cmath>
 using namespace Planner;
 
 ///////////////////////////
@@ -21,7 +22,35 @@ using namespace Planner;
  */
 LocalPlanner::LocalPlanner(const Planner::ParamLocal &l_param,
                            shared_ptr<PlannerBase> p_base_): AbstractPlanner(p_base_),param(l_param){
+    ilqr_param.rho = 1e-5;
+    ilqr_param.drho = 1.0;
+    ilqr_param.rhoFactor = 1.6;
+    ilqr_param.rhoMax = 1e10;
+    ilqr_param.rhoMin = 1e-6;
+    ilqr_param.tolGrads = power(10.0, VectorXd::LinSpaced(30, -4.0, -6.0));
+    ilqr_param.tolCosts = power(10.0, VectorXd::LinSpaced(30, -2.0, -6.0));
+    ilqr_param.tolConsts = power(10.0, VectorXd::LinSpaced(30, -2.0, -6.0));
+    ilqr_param.alphas = power(10.0, VectorXd::LinSpaced(5, 0.0, -3.0));
+    ilqr_param.maxIter = 1000;
+    ilqr_param.mu = 2.0;
+    ilqr_param.lambda = 0.0;
+    ilqr_param.phi = 1e-4;
+    ilqr_param.verbosity = 1;
+    ilqr_param.dmu = 2.0;
+    ilqr_param.dphi = 0.8;
+
+    carDefaultShape << 4.0, 0.0,
+                        0.0, 4.0;
+    for(int i = 0; i<51;i++)
+    {
+        bodyArray[i]<< 4.0, 0.0, 0.0, 4.0;
+    }
+    state_weight_<< 0.05, 0.05, 0.05, 0.05, 0.05;
+    final_weight_<< 0.05, 0.05, 0.05, 0.05, 0.05;
+    input_weight_<< 0.01, 0.01;
+
     cout << "[LocalPlanner] Init." << endl;
+    
 }
 /**
  * Update the planning result to p_base
@@ -29,7 +58,6 @@ LocalPlanner::LocalPlanner(const Planner::ParamLocal &l_param,
 void LocalPlanner::updateTrajToBase() {
     // update routine here
     p_base->setMPCResultTraj(curPlanning); // just an example
-
 }
 /**
  * @brief moniter whether current path is feasible against the obstaclePathArray
@@ -39,42 +67,67 @@ bool LocalPlanner::isCurTrajFeasible() {
 
     return true;
 }
+void LocalPlanner::QxFromPrediction(Collection<double,51> mpcPredictionHeads)
+{
+    Matrix<double,2,2> rotationMatrix;
+    for (int i = 0 ; i <51; i++)
+    {
+        double theta = mpcPredictionHeads[i];
+        rotationMatrix<< cos(theta), -sin(theta),
+                        sin(theta), cos(theta);
+        bodyArray[i] = rotationMatrix * carDefaultShape * rotationMatrix.transpose();
+    }
+}
+void LocalPlanner::ObstToConstraint() {
+    int count_id = 0;
+    Collection<Matrix<double,2,1>,51> path_temp;
+    Collection<Matrix<double,2,2>,51> shape_temp;
+    if(p_base->getCurObstaclePathArray().obstPathArray.size()>0) {
+        for (auto &s : p_base->getCurObstaclePathArray().obstPathArray) {
+            for (int i = 0; i < 51; i++) {
+                path_temp[i].block<2, 1>(0, 0) = s.obstPath[i].q;
+                shape_temp[i].block<2, 2>(0, 0) = s.obstPath[i].Q.inverse();
+            }
+            obs_q.push_back(path_temp);
+            obs_Q.push_back(shape_temp);
+            count_id++;
+        }
+    }
+}
 
 void LocalPlanner::SfcToOptConstraint(){
-    double t_end_= 5.0;
+    double t_end_;
     double t_start_  = 0.0;
     int N_corr = 0;
     int count1 = 0;
     int count2 = 0;
-    for(auto& s: p_base->getCorridorSeq())
-    {
-        if(count1*count2 == 0)
-        {
-            box_constraint[count2] = s;
-            count1++;
-            count2++;
-        }
 
-        t_end_ = s.t_end;
-        if(t_end_ >param.horizon)
-        {
-            for(int i = 0; i< floor((param.horizon-t_start_)/param.tStep);i++)
-            {
+    for(auto &s: p_base->getCorridorSeq()) {
+        if (N_corr < 51) {
+            if (count1 * count2 == 0) {
                 box_constraint[count2] = s;
+                count1++;
                 count2++;
+                N_corr++;
+            }
+
+            t_end_ = s.t_end;
+            if (t_end_ > param.horizon) {
+                for (int i = 0; i < floor((param.horizon - t_start_) / param.tStep); i++) {
+                    box_constraint[count2] = s;
+                    count2++;
+                    N_corr++;
+                }
+            }
+            else {
+                for (int i = 0; i < floor((t_end_ - t_start_) / param.tStep); i++) {
+                    box_constraint[count2] = s;
+                    count2++;
+                    N_corr++;
+                }
+                t_start_ = param.tStep * (count2 - 1);
             }
         }
-        else
-        {
-            for(int i = 0 ; i< floor((t_end_-t_start_)/param.tStep);i++)
-            {
-                box_constraint[count2] = s;
-                count2 ++;
-            }
-            t_start_ = param.tStep * count2;
-
-        }
-
     }
 }
  Collection<Corridor,51> LocalPlanner::getOptCorridor()
@@ -94,49 +147,65 @@ bool LocalPlannerPlain::plan() {
 //    Checked that this function is always executed
 //    cout << "[LocalPlanner] planning... " << endl;
 //    cout << "[LocalPlanner] Done. " << endl;
-
+//    cout<< "I am in the Local Planner plan function"<<endl;
     LocalPlanner::SfcToOptConstraint(); // convert SFC to box constraints
+//    cout<<p_base->getCurObstaclePathArray().obstPathArray[0].obstPath.size()<<endl;
+//    cout<<p_base->getCurObstaclePathArray().obstPathArray[0].obstPath.size()<<endl;
+    //cout<<p_base->getCurObstaclePathArray().obstPathArray[0].obstPath[0].q(0,0)<<endl;
+    LocalPlanner::ObstToConstraint();
     // cout<< LocalPlanner::box_constraint[1].yl<<endl;
     using namespace Eigen;
-    //Following codes will be wrapped with another wrapper;
+//    //Following codes will be wrapped with another wrapper;
 
-    std::shared_ptr<Problem> prob = std::make_shared<Problem>(box_constraint);
-//    // Do not have to be defined every loop
-    Matrix<double,Nx,1> state_weight_;
-    state_weight_.setZero();
-    Matrix<double,Nu,1> input_weight_;
-    input_weight_.setZero();
-    Matrix<double,Nx,1> final_weight_;
-    final_weight_.setZero();
-//
-    bool noConstraint_ = 0;
-    prob->set_state_weight(state_weight_);
-    prob->set_final_weight(final_weight_);
-    prob->set_input_weight(input_weight_);
-    prob->set_noConstraint(noConstraint_);
-//
-//     // A new goal-update function will be defined/added with argument x_goal
-    Matrix<double,2,1> x_goal_;
-    x_goal_.setZero();
-    prob->set_goal(x_goal_);
-    static int loop_num = 0;
-    std::array<Matrix<double,Nu,1>,N> u0;
-    if(loop_num == 0)
-    {
-        for(auto &s :u0)
-        {
-            s=(Matrix<double,Nu,1>()<< 0.0,0.0).finished();
-        }
-    }
+    std::shared_ptr<Problem> prob = std::make_shared<Problem>(bodyArray, box_constraint, obs_Q, obs_q);
+    // Do not have to be defined every loop
+
+     bool noConstraint_ = 0;
+     prob->set_state_weight(state_weight_);
+     prob->set_final_weight(final_weight_);
+     prob->set_input_weight(input_weight_);
+     prob->set_noConstraint(noConstraint_);
+
+////     // A new goal-update function will be defined/added with argument x_goal
+     Matrix<double,2,1> x_goal_;
+     x_goal_<<10.0, 0.0;
+     prob->set_goal(x_goal_);
+     static int loop_num = 0;
+
+     std::array<Matrix<double,Nu,1>,N> u0;
+     if(loop_num == 0)
+     {
+         for(auto &s :u0)
+         {
+             s=(Matrix<double,Nu,1>()<< 0.0,0.0).finished();
+         }
+     }
 
     Matrix<double,Nx,1> x0_new = (Matrix<double,Nx,1>()<<p_base->getCarState().x,
-            p_base->getCarState().y,p_base->getCarState().v,p_base->getCurInput().alpha,p_base->getCarState().theta).finished();
-    Collection<Matrix<double,Nu,1>,N> uN_new = u0;
+            p_base->getCarState().y, p_base->getCarState().v, p_base->getCurInput().alpha, p_base->getCarState().theta).finished();
+    Collection<Matrix<double,Nu,1>,N> uN_new;
 
     if(loop_num == 0)
     {
+        uN_new = u0;
+        iLQR<Nx,Nu,N> ilqr_init(*prob, x0_new,uN_new,dt,ilqr_param);
+        ilqr_init.solve();
+        uN_new = ilqr_init.uN_;
+        for(int j = 0; j<50;j++)
+        {
+            cout<<uN_new[j](0)<<endl;
+        }
 
+        loop_num++;
+        cout<< "Wow No error??"<<endl;
     }
+    else
+    {
+//        iLQR<Nx,Nu,N>ilqr(*prob,x0_new, uN_new,dt,ilqr_param);
+//        ilqr.solve();
+//        cout<<"No error, Congratulations"<<endl;
+    }
+
 
     //TODO: print out the outcome of the planning
 
